815cbb780219bd0ef1659b9d0ccd4b05a366c8a2
gmaheswaranmca <gmaheswaranmca@gmail.com>
2025-02-11 12:34:25 +0530

    go fs

--- ars/02client_design/client/src/App.jsx
+++ ars/02client_design/client/src/App.jsx
@@ -1,12 +1,21 @@
 import FlightList from "./flights/FlightList"
 import FlightCreate from "./flights/FlightCreate"
 import FlightEdit from "./flights/FlightEdit"
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <>     
-     <FlightList/>
-     <FlightCreate/>
-     <FlightEdit/>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="" element={<FlightList/>}/>
+            <Route path="/flights/list" element={<FlightList/>}/>
+            <Route path="/flights/create" element={<FlightCreate/>}/>
+            <Route path="/flights/edit/:id" element={<FlightEdit/>}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   )
 }
--- ars/02client_design/client/src/flights/FlightCreate.jsx
+++ ars/02client_design/client/src/flights/FlightCreate.jsx
@@ -1,7 +1,34 @@
 function FlightCreate() {
     return (
         <>
-        <p>copy here FlightCreate UI</p>
+            <h3><a href="flight_list.html" className="btn btn-light">Go Back</a>New Flight</h3>
+            <div className="container">
+                <div className="form-group mb-3">
+                    <label htmlFor="number" className="form-label">Flight Number:</label>
+                    <input type="text" className="form-control" id="number" placeholder="Please enter flight number"/>
+                </div>
+                <div className="form-group mb-3">
+                    <label htmlFor="airline_name" className="form-label">Airline Name:</label>
+                    <input type="text" className="form-control" id="airline_name" placeholder="Please enter airline name"/>
+                </div>
+                <div className="form-group mb-3">
+                    <label htmlFor="source" className="form-label">Source:</label>
+                    <input type="text" className="form-control" id="source" placeholder="Please enter source"/>
+                </div>
+                <div className="form-group mb-3">
+                    <label htmlFor="destination" className="form-label">Destination:</label>
+                    <input type="text" className="form-control" id="destination" placeholder="Please enter destination"/>
+                </div>
+                <div className="form-group mb-3">
+                    <label htmlFor="capacity" className="form-label">Capacity(Number of Seats):</label>
+                    <input type="text" className="form-control" id="capacity" placeholder="Please enter capacity"/>
+                </div>
+                <div className="form-group mb-3">
+                    <label htmlFor="price" className="form-label">Ticket Price:</label>
+                    <input type="text" className="form-control" id="price" placeholder="Please enter ticket price"/>
+                </div>
+                <button className="btn btn-success">Create Flight</button>
+            </div>
         </>
     );
 }
